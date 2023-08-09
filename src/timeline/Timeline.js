e9bb8f69f5cd5e3a1ed896d92f93523bec517228
Jahyun Choi <119170650+JahyunChoi@users.noreply.github.com>
2023-08-09 18:38:51 +0900

    third commit

--- src/timeline/Timeline.js
+++ src/timeline/Timeline.js
@@ -1,16 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Timeline.css";
 import Sugesstions from './Sugesstions';
 import Post from './posts/Post';
 
 
 function Timeline() {
+  const [posts, setPosts] = useState([
+    {
+      user: "yalruu",
+      poseImage: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+      likes: 12,
+      timestamp: "2d"
+    },
+    {
+      user: "hello_",
+      poseImage: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+      likes: 55,
+      timestamp: "2d"
+    },
+    {
+      user: "itsme",
+      poseImage: "https://images.unsplash.com/photo-1575936123452-b67c3203c357?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80",
+      likes: 29,
+      timestamp: "2d"
+    },
+    
+  ]);
+
+
   return <div className="timeline">
     <div className="timeline__left">
       <div className="timeline__posts">
-        <Post />
-        <Post />
-        <Post />
+        {posts.map((post) => (
+          <Post 
+            user={post.user} 
+            postImage={post.poseImage} 
+            like={post.likes} 
+            timestamp={post.timestamp} 
+          />
+        ))}
       </div>
     </div>
     <div className="timeline__right">
--- src/timeline/posts/Post.js
+++ src/timeline/posts/Post.js
@@ -7,7 +7,7 @@ import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
 import TelegramIcon from "@mui/icons-material/Telegram";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
-function Post() {
+function Post({user, postImage, likes, timestamp}) {
   return (
     <div className='post'>
       <div className="post__header">
@@ -24,13 +24,13 @@ function Post() {
       <div className="post__footer">
         <div className="post__footerIcons">
           <div className="post__iconsMain">
-            <FavoriteBorder className='postIcon' />
-            <ChatBubbleOutlineIcon className='postIcon' />
-            <TelegramIcon className='postIcon' />
+            <FavoriteBorder className="postIcon" />
+            <ChatBubbleOutlineIcon className="postIcon" />
+            <TelegramIcon className="postIcon" />
            
           </div>
           <div className="post__iconsSave">
-            <BookmarkBorderIcon className='postIcon' />
+            <BookmarkBorderIcon className="postIcon" />
           </div>
         </div>
         Liked by 21 people.
